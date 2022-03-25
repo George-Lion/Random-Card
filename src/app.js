4a36c23d7582183bfb22954b5eb393fab6277c1a
Jorge Leon <jorgeleonpicture@gmail.com>
2022-03-25 09:48:13 +0000

    baraja de cartas

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-const cards = ["♥", "♣", "♦", "♠"];
-const numbers = [
+let cards = ["♥", "♣", "♦", "♠"];
+let numbers = [
   "A",
   "2",
   "3",
@@ -18,3 +18,33 @@ const numbers = [
   "Q",
   "K"
 ];
+
+let newCards = [];
+
+for (let value of numbers) {
+  for (let stick of cards) {
+    newCards.push([value] + [stick]);
+  }
+}
+
+newCards = random(newCards);
+
+//desordenar
+
+function random(Array) {
+  var t = Array.sort(function(a, b) {
+    return Math.random() - 0.5;
+  });
+  return [...t];
+}
+
+console.log(a);
+
+// reset
+
+function borrar() {
+  a = random(a);
+  position = 0;
+}
+
+console.log(a);
