5af4ca1861df2743bc836253bf27797be496457c
Jorge Leon <jorgeleonpicture@gmail.com>
2022-03-28 15:32:10 +0000

    actualización prueba de funcionalidad

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
+import "@popperjs/core";
 import "bootstrap";
 import "./style.css";
 
 let newCards = [];
 let position = 1;
-let cards = ["♥", "♣", "♦", "♠"];
-let numbers = [
+let distribute;
+const cards = ["♥", "♣", "♦", "♠"];
+const numbers = [
   "A",
   "2",
   "3",
@@ -20,31 +22,63 @@ let numbers = [
   "Q",
   "K"
 ];
-for (let value of numbers) {
-  for (let stick of cards) {
-    let x = [value, stick];
-    newCards.push(x);
-    newCards.sort(() => Math.random() - 0.5);
+const num = document.querySelector("#number");
+const stickUp = document.querySelector("#stick1");
+const stickFooter = document.querySelector("#stick2");
+const message = document.querySelector("#mensaje");
+
+const inicio = () => {
+  for (let value of numbers) {
+    for (let stick of cards) {
+      let arr = [value, stick];
+      newCards.push(arr);
+      newCards.sort(() => Math.random() - 0.5);
+    }
   }
-}
-function deckOfCards() {
-  let distribute = newCards[position];
-  document.querySelector("#number").innerHTML = distribute[0];
-  document.querySelector("#stick1").innerHTML = distribute[1];
-  document.querySelector("#stick2").innerHTML = distribute[1];
+  return newCards;
+};
+
+inicio();
+
+const random = newCards => {
+  newCards.sort(() => Math.random() - 0.5);
+  return newCards;
+};
+
+const deckOfCards = () => {
+  reset.disabled = false;
+  distribute = newCards[position];
+  num.innerHTML = distribute[0];
+  stickUp.innerHTML = distribute[1];
+  stickFooter.innerHTML = distribute[1];
 
   position = position + 1;
   if (position == newCards.length - 1) {
-    document.querySelector("#number").innerHTML = "X";
-    document.querySelector("#stick1").innerHTML = "X";
-    document.querySelector("#stick2").innerHTML = "X";
+    num.innerHTML = "X";
+    stickUp.innerHTML = "";
+    stickFooter.innerHTML = "";
     start.disabled = true;
+    alert("EMPTY");
+    message.innerHTML = "press reset to continue";
   }
-}
+};
 
-function newTT() {
-  newCards = a(newCards);
+const resetCards = () => {
+  newCards = random(newCards);
   position = 0;
-}
+  num.innerHTML =
+    "<img src=https://media2.giphy.com/media/JRluhfVtaCfKcq7Kxf/giphy.gif?cid=ecf05e4757b2hioh1zzw43dqrl745dk694iq080e60nct151&rid=giphy.gif&ct=g width=200px/>";
+  stickUp.innerHTML = "";
+  stickFooter.innerHTML = "";
+  start.disabled = false;
+  reset.disabled = true;
+  message.innerHTML = "press start";
+};
+
 start.addEventListener("click", deckOfCards);
-/* reset.addEventListener("click", nuevoMazo); */
+reset.addEventListener("click", resetCards);
+
+window.onload = function() {
+  reset.disabled = true;
+  message.innerHTML = "press start";
+};
